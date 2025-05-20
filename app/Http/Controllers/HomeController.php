8184e862d9b79288923f191ca5498bb06052e8ef
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2025-05-20 11:47:55 +0700

    interface pairing category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Slider;
+use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -12,7 +14,10 @@ class HomeController extends Controller
     public function index()
     {
         $sliders = Slider::latest()->get();
-        return view('home.home', compact('sliders'));
+        // DB::connection()->enableQueryLog(); kiểm tra câu lệnh query
+        $categories = Category::where('parent_id', 0)->get();
+        // $queries = DB::getQueryLog();
+        return view('home.home', compact(['sliders', 'categories']));
     }
     public function test()
     {
