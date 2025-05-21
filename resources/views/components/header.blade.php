d1ba2c14f5985177cbc2de79e8c021e8c061d589
phucvan101 <111259337+phucvan101@users.noreply.github.com>
2025-05-21 10:00:16 +0700

    interface pairing setting

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,19 +5,23 @@
                 <div class="col-sm-6">
                     <div class="contactinfo">
                         <ul class="nav nav-pills">
-                            <li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
-                            <li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
+                            <li><a href="#"><i class="fa fa-phone"></i>{{getConfigValueFromSettingTable('phone_contact')}}</a></li><!--composer dump-autoload cập nhật lại composer.json để tạo đường dẫn helper-->
+                            <li><a href="#"><i class="fa fa-envelope"></i> {{getConfigValueFromSettingTable('email')}}</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="social-icons pull-right">
                         <ul class="nav navbar-nav">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li>
+                                <a href="{{getConfigValueFromSettingTable('facebook_link')}}"><i class="fa fa-facebook"></i></a>
+                            </li>
+
+                            <li>
+                                <a href="{{getConfigValueFromSettingTable('linkendin_link')}}"><i class="fa fa-linkedin"></i></a>
+                            </li>
+
+
                         </ul>
                     </div>
                 </div>
--- resources/views/home/components/category_tab.blade.php
+++ resources/views/home/components/category_tab.blade.php
@@ -19,7 +19,7 @@
                     <div class="single-products">
                         <div class="productinfo text-center">
                             <img src="{{config('app.base_url') . $productItemTabs->feature_image_path}}" alt="" />
-                            <h2>{{$productItemTabs->price}}</h2>
+                            <h2>{{number_format($productItemTabs->price)}} VND</h2>
                             <p>{{$productItemTabs->name}}</p>
                             <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
                         </div>
